689535e0c5d4f87b689ffcc2b692f5722350532b
Connor Leech <connorleech@gmail.com>
2015-06-24 23:49:19 -0700

    add email sender

---
+++ server/email/sendEmail.js
@@ -0,0 +1,28 @@
+var nodemailer = require('nodemailer');
+var mandrillTransport = require('nodemailer-mandrill-transport');
+var MANDRILL_API_KEY = 'ze2Lcwew5KrZkSNCrfUQcw';
+
+// object that is able to send mail
+// You can use the same transporter object for all e-mails (no need to recreate)
+var transport = nodemailer.createTransport(mandrillTransport({
+  auth: {
+    apiKey: MANDRILL_API_KEY
+  }
+}));
+
+var sendEmail = function(mailObject){
+    transport.sendMail({
+        from: 'claydshaw@gmail.com',
+        to: mailObject.to,
+        subject: mailObject.subject,
+        html: mailObject.html
+    }, function(err, info) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(info);
+      }
+    });
+};
+
+module.exports = sendEmail;
